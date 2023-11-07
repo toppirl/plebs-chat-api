93754c5bc88ad9b0242dc70e0d5f65d7e69b9fca
Jordan Topp <toppdat@gmail.com>
2023-11-06 17:34:21 -0800

    Updated java main

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 document.querySelector('button').addEventListener('click', apiRequest)
 
 async function apiRequest() {
-    const name = document.querySelector('input').value
-    try {
-        const response = await fetch(`https://shy-tan-elk-robe.cyclic.app/api/${name}`)
-        const data = await response.json()
-
-        document.querySelector('h3').innerText = data.occupation
-    } catch(error) {
-        console.log(error)
-    }
+  const name = document.querySelector('input').value
+  try {
+    const response = await fetch(
+      `https://spectrum-assembly-api.onrender.com/api/${name}`
+    )
+    const data = await response.json()
+    document.querySelector('h3').innerText = data.email
+  } catch (error) {
+    console.log(error)
+  }
 }
